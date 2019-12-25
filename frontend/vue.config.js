c3fca89e86911a2cd8caedbd424489f42d1d38a8
rzhannoy <nrzhannoy@gmail.com>
2019-12-25 09:42:41 +0700

    fe: add gzipping

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,17 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   outputDir: 'dist/new',
   devServer: {
     proxy: {
       '/media': { target: 'http://127.0.0.1:8000/media' },
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        test: /\.(js|css)$/,
+      }),
+    ],
   },
 }
